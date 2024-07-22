3324f204355b24bad016cf2d49edb289ad5c0fb7
Camilobarre <barrecami@gmail.com>
2024-07-22 08:23:32 -0500

    Se crea el repositorio

---
+++ Models/Contador.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace POO_staticEjercicios.Models;
+public class Contador
+{
+    public static int TotalContadores = 0;
+    
+}
